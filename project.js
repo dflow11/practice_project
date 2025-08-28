9adb0af51642afb91d672ef5ef1deeeb931bf90d
Diego <165971972+dflow11@users.noreply.github.com>
2025-08-28 16:57:19 -0500

    Project Setup: initialized node.js, prompt-sync for user input in terminal

---
+++ project.js
@@ -0,0 +1,8 @@
+// 1. Deposit some money
+// 2. Determine amount of lines to bet on
+// 3. Collect bet amount
+// 4. Spin the slot machine
+// 5. Check if player won
+// 6. Give the user the winnings.
+// 7. Play again or Prompt the user they're out of funds.
+
